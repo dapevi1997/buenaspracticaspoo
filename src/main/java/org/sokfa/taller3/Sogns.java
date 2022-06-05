de167e10ede50ae4af05eadf8cc2ff111679d21b
dapevi1997 <dapevi97@gmail.com>
2022-06-04 19:38:29 -0500

    Clase padre Canciones creada

---
+++ src/main/java/org/sokfa/taller3/Sogns.java
@@ -0,0 +1,121 @@
+package org.sokfa.taller3;
+
+import java.util.Calendar;
+
+/**
+ * Clase padre para la creación de canciones.
+ *
+ * @version 1.0.0 2022-06-04
+ *
+ * @author DANIEL PEREZ VITOLA - dapevi97@gmail.com
+ *
+ * @since 1.0.0
+ *
+ */
+public class Sogns {
+
+    /**
+     * Representa la fecha en que fue lanzada la canción.
+     */
+    private Calendar date;
+    /**
+     * Representa el id único de cada canción.
+     */
+    private String id;
+    /**
+     * Representa el título de la canción.
+     */
+    private String title;
+    /**
+     * Representa la duración de la canción.
+     */
+    private double duration;
+    /**
+     * Representa el género de la canción.
+     */
+    private String gender;
+    /**
+     * Representa el nombre de la imagen de la portada del disco.
+     */
+    private String cover;
+    /**
+     * Representa una corta descripción de la canción.
+     */
+    private String description;
+
+    /**
+     * Constructor de la clase padre.
+     *
+     * @param date Feha de lanzamiento de la canción.
+     * @param id Id de la canción.
+     * @param title Título de la canción.
+     * @param duration Duración de la canción.
+     * @param cover Nombre de la imagen de la carátula del album.
+     * @param description Descripción de la canción.
+     */
+    public Sogns(String id, String title, Calendar date, double duration,
+            String cover, String description) {
+        this.date = date;
+        this.id = id;
+        this.title = title;
+        this.duration = duration;
+        this.cover = cover;
+        this.description = description;
+    }
+
+    /**
+     * Método para obtener la fecha de una canción.
+     *
+     * @return Fecha de lanzamiento de la canción.
+     */
+    public Calendar getDate() {
+        return date;
+    }
+
+    /**
+     * Método para obtener el id de una canción.
+     *
+     * @return Id de la canción.
+     */
+    public String getId() {
+        return id;
+    }
+
+    /**
+     * Método para obtener la duración en minutos de una canción.
+     *
+     * @return Duración de la canción.
+     */
+    public double getDuration() {
+        return duration;
+    }
+
+    /**
+     * Método para obtener el género de una canción.
+     *
+     * @return Género de la canción.
+     */
+    public String getGender() {
+        return gender;
+    }
+
+    /**
+     * Sobreescritura del método toString para mostrar información de la
+     * canción.
+     *
+     * @return Cadena de texto de información acerca de la canción.
+     */
+    @Override
+    public String toString() {
+        return "ID: " + id + " "
+                + "Título: " + title + " "
+                + "Género: " + gender + " "
+                + "Fecha: " + (date.get(Calendar.YEAR)) + "/"
+                + (date.get(Calendar.MONTH) + 1) + "/"
+                + date.get(Calendar.DAY_OF_MONTH) + " "
+                + "Duración: " + duration + " "
+                + "Carátula: " + cover + " "
+                + "Descripción: " + description;
+    }
+
+}
